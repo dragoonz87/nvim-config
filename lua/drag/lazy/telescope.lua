0b051a7e45dbdeaec6fd71ffce80d868d929038c
Sam Bersch <sammybersch@gmail.com>
2024-11-23 15:30:00 -0500

    fixed telescope grep_string

--- lua/drag/lazy/telescope.lua
+++ lua/drag/lazy/telescope.lua
@@ -5,11 +5,23 @@ return {
         local ivy_theme = require("telescope.themes").get_ivy({})
         local builtin = require('telescope.builtin')
         require("which-key").add({
-            { "<leader>sff", function() builtin.find_files(ivy_theme) end,                              desc = "[F]ind [f]ile" },
-            { "<leader>sfg", function() builtin.git_files(ivy_theme) end,                               desc = "[F]ind [g]it files" },
-            -- { "<leader>ss",  function() builtin.grep_string({ search = vim.fn.input("Grep > "), }) end, desc = "[F]ind [s]tring" },
-            { "<leader>ss",  function() builtin.grep_string(ivy_theme) end, desc = "[F]ind [s]tring" },
-            { "<leader>sg",  function() builtin.live_grep(ivy_theme) end,                               desc = "[G]rep" }
+            { "<leader>sff", function() builtin.find_files(ivy_theme) end, desc = "[F]ind [f]ile" },
+            { "<leader>sfg", function() builtin.git_files(ivy_theme) end,  desc = "[F]ind [g]it files" },
+            { "<leader>sg",  function() builtin.live_grep(ivy_theme) end,  desc = "[G]rep" },
+            {
+                mode = "n",
+                "<leader>ss",
+                function()
+                    vim.ui.input({ prompt = "Grep" }, function(input)
+                        if input == nil or input == "" then
+                            return
+                        end
+                        builtin.grep_string({ search = input, theme = ivy_theme })
+                    end)
+                end,
+                desc = "[F]ind [s]tring"
+            },
+            { mode = "v", "<leader>ss", function() builtin.grep_string(ivy_theme) end, desc = "[F]ind [s]tring" },
         })
     end
 }
